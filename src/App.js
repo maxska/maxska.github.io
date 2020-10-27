c1392439e4bc86b2490bff57e4c0ea158b02a3ab
Max <maxskan98@gmail.com>
2020-10-27 20:22:26 +0100

    Replaced the auto-generated stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Startpage from './Startpage.js'
+
+function App()
+{
+  return(
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="">
+            <Startpage/>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Startpage.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+const Startpage = () =>
+{
+    return(
+        <div>
+            <h1>STARTPAGE</h1>
+
+        </div>
+    )
+}
+
+export default Startpage;
