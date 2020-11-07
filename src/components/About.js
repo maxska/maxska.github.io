3194ad419c2d4e8e6b928505e9b8cb0795799a89
Max <maxskan98@gmail.com>
2020-11-07 01:36:53 +0100

    almost done with inner navbar

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,20 @@ import './style/About.css';
 import TopBar from './TopBar.js';
 import AboutText from './texts/AboutText.js';
 
+import {useLocation} from 'react-router-dom';
+
+
+
 const About = () =>
 {
+	const setActiveTab = (page) => 
+	{
+		document.getElementById(page).classList.add("dynamic-style")
+
+		let otherPage = page === "work" ? "education" : "work";
+		document.getElementById(otherPage).classList.remove("dynamic-style");
+	}
+
 	return(
 		<div>
 
@@ -19,11 +31,11 @@ const About = () =>
 				<Router>
 				
 					<div className="inner-navbar">
-						<Link to="/about/work" className="inner-navbar-element">
+						<Link to="/about/work" className="inner-navbar-element" id="work" onClick={() => setActiveTab("work")}>
 							Work & Student work
 						</Link>
 
-						<Link to="/about/education" className="inner-navbar-element">
+						<Link to="/about/education" className="inner-navbar-element" id="education" onClick={() => setActiveTab("education")}>
 							Education
 						</Link>
 					</div>
