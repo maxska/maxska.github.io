268632c92fb999b198be506143435becde01bf20
Max <55145161+maxska@users.noreply.github.com>
2021-04-05 18:04:24 +0200

    changed Projects table design

--- src/components/ProjectElement.js
+++ src/components/ProjectElement.js
@@ -1,63 +1,59 @@
+const ProjectElement = (props) => {
+	return (
+		<div className='project-element'>
+			<h2 style={{ color: "#e4b271", textAlign: "center" }}>{props.title}</h2>
+			<h4>
+				<span style={{ color: "#e4b271" }}>Made in: </span>
+				{props.languages}
+			</h4>
 
-const ProjectElement = (props) =>
-{
-  return(
-    <div className="project-element">
-      <h2 style={{color: "#ffd574", textAlign: "center"}}>{props.title}</h2>
-      <h4><span style={{color: "#ffd574"}}>Made in: </span>{props.languages}</h4>
+			{props.workInProgress && (
+				<div
+					style={{
+						border: "2px solid #3d4f58",
+						padding: "10px",
+						marginRight: "25px",
+						marginLeft: "25px",
+						borderRadius: "5px",
+					}}
+				>
+					{props.workInProgress}
+				</div>
+			)}
 
-      {
-        props.workInProgress
-        &&
-        <div style={{
-          backgroundColor: "#48555c", 
-          border: "1px solid orange",
-          padding: "10px",
-          marginRight: "25px",
-          marginLeft: "25px",
-          borderRadius: "5px"
-        }}>
-          {props.workInProgress}
-        </div>
-      }
+			{props.description && (
+				<p>
+					<span style={{ color: "#e4b271" }}>Description: </span>
+					{props.description}
+				</p>
+			)}
 
+			<p>
+				<a href={props.repo} target='_blank' rel='noopener noreferrer'>
+					GitHub repo
+				</a>
+			</p>
 
-      {
-        props.description
-        &&
-        <p>
-          <span style={{color: "#ffd574"}}>Description: </span>
-          {props.description}
-        </p>
-      }
+			{props.liveDemo && (
+				<a href={props.liveDemo} target='_blank' rel='noopener noreferrer'>
+					Live demo of this project
+				</a>
+			)}
 
-      <p>
-        <a href={props.repo} target="_blank" rel="noopener noreferrer">GitHub repo</a>
-      </p>
-      
-      {
-        props.liveDemo 
-        &&
-        <a href={props.liveDemo} target="_blank" rel="noopener noreferrer">Live demo of this project</a>
-      }
-
-      {
-        props.image 
-        &&
-        <img 
-        src={props.image} 
-        alt={props.title + " image"}
-        style={{
-          display: "block",
-          width: "250px",
-          margin: "auto",
-          padding: "5px"
-        }}
-        />
-      }
-
-    </div>
-  )
-}
+			{props.image && (
+				<img
+					src={props.image}
+					alt={props.title + " image"}
+					style={{
+						display: "block",
+						width: "250px",
+						margin: "auto",
+						padding: "5px",
+					}}
+				/>
+			)}
+		</div>
+	);
+};
 
 export default ProjectElement;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
-import './style/Home.css';
+import "./style/Home.css";
 
 import "./style/ProjectsText.css";
 import ProjectsTexts from "./texts/Projects_texts.json";
-import ProjectElement from "./ProjectElement.js"
+import ProjectElement from "./ProjectElement.js";
 
 import thumbnail_calculator from "./texts/images/thumbnail_calculator.png";
 import thumbnail_gradecalc from "./texts/images/thumbnail_gradecalc.png";
@@ -15,41 +15,32 @@ let images = {
 	image_calculator: thumbnail_calculator,
 	image_gradecalc: thumbnail_gradecalc,
 	image_organization: thumbnail_organization,
-  image_unitconverter: thumbnail_unitconverter,
-  image_separatorgenerator: thumbnail_separatorgenerator
-}
+	image_unitconverter: thumbnail_unitconverter,
+	image_separatorgenerator: thumbnail_separatorgenerator,
+};
 
-const Projects = () =>
-{
-	return(
+const Projects = () => {
+	return (
 		<div>
-      
-			<div className="page">
-				<h1 className="heading">Projects</h1>
+			<div className='page'>
+				<h1 className='heading'>Projects</h1>
 
-				<p>
-					{ProjectsTexts["info"]}
-				</p>
+				<p>{ProjectsTexts["info"]}</p>
 
-				{
-					ProjectsTexts["projects"].map(project => {
-						return (
-							<ProjectElement 
-							title={project.title}
-							languages={project.languages}
-							repo={project.repo}
-							liveDemo={project.liveDemo}
-							description={project.description}
-							workInProgress={project.workInProgress}
-							image={images[project.image]}
-							/>
-						);
-					})
-				}
+				{ProjectsTexts["projects"].map((project) => (
+					<ProjectElement
+						title={project.title}
+						languages={project.languages}
+						repo={project.repo}
+						liveDemo={project.liveDemo}
+						description={project.description}
+						workInProgress={project.workInProgress}
+						image={images[project.image]}
+					/>
+				))}
 			</div>
-
 		</div>
-	)
-}
+	);
+};
 
 export default Projects;
